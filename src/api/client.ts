0f96456af93378282145e0b63a564d9759c152bb
Makhubele Langavi <mlangaviclyde@gmail.com>
2026-05-07 08:37:59 +0200

    Endpoint fix

--- src/api/client.ts
+++ src/api/client.ts
@@ -6,7 +6,7 @@ import axios, {
 } from 'axios'
 
 
-const BASE_URL = process.env.BACKEND_URL || 'http://localhost:8000/api/v1'
+const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000/api/v1'
 const TOKEN_KEY = 'phila_practice_token'
 
 const apiClient: AxiosInstance = axios.create({
